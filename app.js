74d2b84ce720173b8d74ec7f1487d82acbd7e4e5
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-03-31 12:35:37 -0700

    added working gif search

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 // libraries
 const express = require('express');
 const exphbs = require('express-handlebars')
+require("dotenv").config();
 
 // app setup
 const app = express();
+
+// middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars')
 
-// middleware
+// tenor
+const Tenor = require('tenorjs').client({
+    "Key": process.env.TENOR_API_KEY,
+    "Filter": "high",
+    "Locale": "en_US",
+});
 
 // routes
 app.get('/', (req, res) => {
@@ -15,8 +23,15 @@ app.get('/', (req, res) => {
     // const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245';
     // render the hello-gif view, passing the gifUrl into the view to be displayed
     // res.render('hello-gif', { gifUrl })
-    console.log(req.query)
-    res.render('home')
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            const gifs = response;
+            res.render('home', { gifs })
+        }).catch(console.error)
 });
 
 app.get('/greetings/:name', (req, res) => {
