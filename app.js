54476ee2ace09af84be11aac33450030f9bf71e8
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-03-31 10:54:03 -0700

    added middleware

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // libraries
 const express = require('express');
+const exphbs = require('express-handlebars')
 
 // app setup
 const app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars')
 
 // middleware
 
 // routes
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    // set the gif url
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245';
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    res.render('hello-gif', { gifUrl })
+});
+
+app.get('/greetings/:name', (req, res) => {
+    // grab the name from the path provided
+    const name = req.params.name;
+    // render the greetings view with the name
+    res.render('greetings', { name });
 });
 
 // start server
