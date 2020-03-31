f1931dac3f74b7951683a7a4a4c30141dc22d7e6
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-03-31 13:13:14 -0700

    adding working gif search through https

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // libraries
 const express = require('express');
 const exphbs = require('express-handlebars')
+const https = require("https");
 require("dotenv").config();
 
 // app setup
@@ -23,15 +24,37 @@ app.get('/', (req, res) => {
     // const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245';
     // render the hello-gif view, passing the gifUrl into the view to be displayed
     // res.render('hello-gif', { gifUrl })
+
     term = ""
     if (req.query.term) {
         term = req.query.term
     }
-    Tenor.Search.Query(term, "10")
-        .then(response => {
-            const gifs = response;
-            res.render('home', { gifs })
-        }).catch(console.error)
+
+    // pulling gifs from tenor js wrapper
+    // Tenor.Search.Query(term, "10")
+    //     .then(response => {
+    //         const gifs = response;
+    //         res.render('home', { gifs })
+    //     }).catch(console.error)
+    
+    // pulling gifs from tenor api via http
+    https.get(`https://api.tenor.com/v1/search?key=${process.env.TENOR_API_KEY}&q=${term}`, (response) => {
+        let data = "";    
+
+        // a chunk of data has been received
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // the whole response has been received, return the result
+        response.on('end', () => {
+            let gifs = JSON.parse(data).results;
+            res.render("home", { gifs });
+        });
+    }).on('error', (err) => {
+        console.log("Error: " + err.message);
+    });
+
 });
 
 app.get('/greetings/:name', (req, res) => {
